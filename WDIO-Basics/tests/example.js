8dded4c8700d68f3f517ef42ece819275485179b
Martin <marb61a@gmail.com>
2020-11-25 21:05:46 +0000

    Updating example test and notes files

--- WDIO-Basics/tests/example.js
+++ WDIO-Basics/tests/example.js
@@ -1,6 +1,16 @@
 describe('First Steps with WebdrverIO', () => {
     it("Load example website", () => {
-        browser.url('www.example.com');
+        browser.url('http://www.example.com');
+        browser.pause(3000);
+
+        expect(browser).toHaveUrl('http://www.example.com/');
+        expect(browser).toHaveTitle('Example Domain');
+
     });
     
+    // it("Load Webdriver website", () => {
+    //     browser.url('https://webdriver.io');
+    //     browser.pause(3000);
+    // });
+
 });
