3d90b539bbffe15ad4a5f7e7cb2dc59df94bb55b
Martin <marb61a@gmail.com>
2020-11-26 12:07:39 +0000

    Updated example.js test file

--- WDIO-Basics/tests/example.js
+++ WDIO-Basics/tests/example.js
@@ -16,19 +16,42 @@ describe('First Steps with WebdrverIO', () => {
     it('H1 should be visible', () => {
         // Gets the selector and saves to variable
         const h1 = $('h1');
-
+        h1.waitForExist();
         expect(h1).toBeVisible();
     });
 
     it('P tag should be visible', () => {
         const p = $('p');
-
+        p.waitForExist();
         expect(p).toBeVisible();
     });
 
     it('Check Link Value', () => {
         const link = $('a');
         expect(link).toHaveLink('https://www.iana.org/domains/example')
+    });
+
+    it('Get Element Text', () => {
+        const text = $('h1').getText();
+
+        const element = $('h1');
+        element.waitForExist();
+        expect(element).toHaveText('Example Domain');
+    });
+
+    it('Assert Attribute', () => {
+        browser.url('https://devexpress.github.io/testcafe/example/');
+
+        const button = $('#submit-button');
+        button.waitForExist();
+        expect(button).toHaveAttrContaining('type', 'submit');
+    });
+
+    it('Assert Value', () => {
+        const button = $('#populate');
+
+        button.waitForExist();
+        expect(button).toHaveValue('Populate');
     });
 
 });
