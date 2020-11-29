1c2b6b023bce9a8e4b1f0d79c352eb37a2ae3578
Martin <marb61a@gmail.com>
2020-11-29 20:00:27 +0000

    Added e2e currency exchange test

--- WDIO-Basics/wdio.conf.js
+++ WDIO-Basics/wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     ],
     // Patterns to exclude.
     exclude: [
-        './tests/example.js', './tests/actions.js'
+        './tests/example.js', './tests/actions.js', './tests/e2e-login.js', './tests/e2e-search.js', './tests/e2e-feedback.js'
     ],
     //
     // ============
