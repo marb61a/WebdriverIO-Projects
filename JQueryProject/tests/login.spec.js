cdef0e3c776be2ba99bcba8ff31bb9a2393a8d82
Martin <marb61a@gmail.com>
2020-12-09 12:24:02 +0000

    Updated login spec file tests

--- JQueryProject/tests/login.spec.js
+++ JQueryProject/tests/login.spec.js
@@ -11,19 +11,44 @@ describe('Test Suite', () => {
 
         assert.equal(browser.isAlertOpen(), true, 'Alert is not open');
         assert.equal(browser.getAlertText(), 'Please enter an email and password', 'Alert text not equal');
+        browser.acceptAlert();
+
+        assert.equal(browser.isAlertOpen(), false, 'Alert is still open');
 
         browser.pause(2000);
     });
     
     it('Should display error when email is missing', () => {
+        browser.url('');
 
+        LoginPage.passwordField.setValue('blah');
+        LoginPage.submitButton.click();
+
+        assert.equal(browser.isAlertOpen(), true, 'Alert is not open');
+        assert.equal(browser.getAlertText(), 'Please enter an email and password', 'Alert text not equal');
+        browser.acceptAlert();
+
+        assert.equal(browser.isAlertOpen(), false, 'Alert is still open');
+        browser.pause(2000);
     });
 
     it('Should display error when email and password are missing', () => {
+        browser.url('');
+
+        LoginPage.submitButton.click();
+
+        assert.equal(browser.isAlertOpen(), true, 'Alert is not open');
+        assert.equal(browser.getAlertText(), 'Please enter an email and password', 'Alert text not equal');
+        browser.acceptAlert();
+
+        assert.equal(browser.isAlertOpen(), false, 'Alert is still open');
+        browser.pause(2000);
+    });
+
+    it('Should display error when email is incorrect', () => {
 
     });
 
-    it('Should display error when email is incorrect');
     it('Should display error when password is incorrect');
     it('Should display error when password case is incorrect');
     it('Should login with valid email and passord');
