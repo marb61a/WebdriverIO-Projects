b5a4c16c4f1ebafe6f311ee4fa404e8fe646574d
Martin <marb61a@gmail.com>
2020-12-11 19:48:02 +0000

    Updated header spec file modal tests

--- JQueryProject/tests/header.spec.js
+++ JQueryProject/tests/header.spec.js
@@ -17,11 +17,32 @@ describe('Header Test Suite', () => {
         browser.pause(2000);
         HeaderPage.linkLink.click();
 
-        // assert.equal(browser.getUrl(), 'https://glitchitsystem.com/');
-        // browser.pause(2000);
+        assert.equal(browser.getUrl(), 'https://glitchitsystem.com/');
+        browser.pause(2000);
     });
 
-    it('It should test something else', () => {
+    it.only('It should open wolverine modal', () => {
+        browser.url('');
+
+        browser.maximizeWindow();
+
+        LoginPage.emailField.setValue('1@2.com');
+        LoginPage.passwordField.setValue('password');
+        LoginPage.rememberLoginCheckbox.click();
+        LoginPage.submitButton.click();
+
+        HeaderPage.heroFactsLink.click();
+        HeaderPage.wolverineOption.click();
+
+        browser.pause(2000);
+
+        assert.equal(HeaderPage.wolverineModalWindow.isDisplayed(), true, 'Modal is not displayed');
+        assert.equal(HeaderPage.wolverineModalTitleText.getText(), 'Wolverine Fact', 'There is a title mismatch');
+        assert.equal(HeaderPage.wolverineModalContentText.getText(), 'Wolverine made his first comic book appearance in 1974.', 'Content does not match');
+
+    });
+
+    it('It should open wolverine modal', () => {
 
     });
 
