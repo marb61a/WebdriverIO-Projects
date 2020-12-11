dee446c0a14632e88373ab09d677d253f4c6f072
Martin <marb61a@gmail.com>
2020-12-11 12:34:36 +0000

    Updated header spec file

--- JQueryProject/tests/header.spec.js
+++ JQueryProject/tests/header.spec.js
@@ -1,9 +1,28 @@
 const assert = require('assert');
 
+const LoginPage = require('../pages/login.page');
 const HeaderPage = require('../pages/header.page');
 
-describe('Test Suite', () => {
-    it('Should test something');
-    it('Should test something else');
+describe('Header Test Suite', () => {
+    it('It should redirect to new site', () => {
+        browser.url('');
+
+        browser.maximizeWindow();
+
+        LoginPage.emailField.setValue('1@2.com');
+        LoginPage.passwordField.setValue('password');
+        LoginPage.rememberLoginCheckbox.click();
+        LoginPage.submitButton.click();
+
+        browser.pause(2000);
+        HeaderPage.linkLink.click();
+
+        // assert.equal(browser.getUrl(), 'https://glitchitsystem.com/');
+        // browser.pause(2000);
+    });
+
+    it('It should test something else', () => {
+
+    });
 
 });
