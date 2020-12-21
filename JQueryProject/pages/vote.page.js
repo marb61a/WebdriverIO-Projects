4ae6f1055b21ba5c63292759474d1d36bc15e1cb
Martin <marb61a@gmail.com>
2020-12-21 19:51:55 +0000

    Added vote page objects

--- JQueryProject/pages/vote.page.js
+++ JQueryProject/pages/vote.page.js
@@ -1,5 +1,94 @@
 class VotePage {
+    get voteTitle(){
+        return $("");
+    }
 
+    // Radio Buttons
+    get voteItem1(){
+        return $("");
+    }
+
+    get voteItem2(){
+        return $("");
+    }
+
+    get voteItem3(){
+        return $("");
+    }
+
+    get voteItem4(){
+        return $("");
+    }
+
+    get voteItem5(){
+        return $("");
+    }
+
+    // Labels
+    get voteItemLabel1(){
+        return $("");
+    }
+
+    get voteItemLabel2(){
+        return $("");
+    }
+
+    get voteItemLabel3(){
+        return $("");
+    }
+
+    get voteItemLabel4(){
+        return $("");
+    }
+
+    get voteItemLabel5(){
+        return $("");
+    }
+
+    get submitButton(){
+        return $("");
+    }
+
+    // Movie title grid / Number of votes
+    get voteItemText1(){
+        return $("");
+    }
+
+    get voteItemVal1(){
+        return $("");
+    }
+
+    get voteItemText2(){
+        return $("");
+    }
+
+    get voteItemVal2(){
+        return $("");
+    }
+
+    get voteItemText3(){
+        return $("");
+    }
+
+    get voteItemVal3(){
+        return $("");
+    }
+
+    get voteItemText4(){
+        return $("");
+    }
+
+    get voteItemVal4(){
+        return $("");
+    }
+
+    get voteItemText5(){
+        return $("");
+    }
+
+    get voteItemVal5(){
+        return $("");
+    }
 }
 
 module.exports = new VotePage();
