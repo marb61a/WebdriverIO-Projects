0188a1fac00212cd1886194746dfca949908dd9d
Martin <marb61a@gmail.com>
2020-12-21 12:10:44 +0000

    Adding roster page objects

--- JQueryProject/pages/roster.page.js
+++ JQueryProject/pages/roster.page.js
@@ -1,4 +1,44 @@
 class RosterPage {
+    get instructionText(){
+        return $("body > div.container-fluid > div:nth-child(3) > p");
+    }
+
+    get listTitle(){
+        return $("body > div.container-fluid > div:nth-child(4) > h3");
+    }
+
+    // Creating each element individual
+    get wolverineItem(){
+        return $("");
+    }
+
+    get ironManItem(){
+        return $("");
+    }
+
+    get deadpoolItem(){
+        return $("");
+    }
+
+    get thorItem(){
+        return $("");
+    }
+
+    get spidermanItem(){
+        return $("");
+    }
+
+    get addHeroLabel(){
+        return $("");
+    }
+
+    get addHeroField(){
+        return $("");
+    }
+
+    get submit(){
+        return $("");
+    }
 
 }
 
