ca9a5cc45ba000c9038ce96a2d9f70eed81934de
Martin <marb61a@gmail.com>
2020-12-21 12:03:09 +0000

    Updated intro spec test file

--- JQueryProject/tests/intro.spec.js
+++ JQueryProject/tests/intro.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const LoginPage = require('../pages/login.page');
+const IntroPage = require("../pages/intro.page");
 
 describe("Intro Test Suite", () => {
     it("Should display correct title", () => {
@@ -13,6 +14,25 @@ describe("Intro Test Suite", () => {
         LoginPage.rememberLoginCheckbox.click();
         LoginPage.submitButton.click();
 
+        assert.equal(IntroPage.titleText.getText(), "Superhero Roster", "Text does not match");
+
+        browser.pause(2000);
+    });
+
+    it.only("Should display correct image", () => {
+        browser.url('');
+
+        browser.maximizeWindow();
+
+        LoginPage.emailField.setValue('1@2.com');
+        LoginPage.passwordField.setValue('password');
+        LoginPage.submitButton.click();
+
+        // Cannot use relative source for image path, it must be absolute
+        assert.equal(IntroPage.mainImage.getAttribute("src"), "http://localhost:8080/images/superhero.png", "Src differs");
+        assert.equal(IntroPage.mainImage.getAttribute("alt"), "Superhero Image", "Alt differs");
+
+        browser.pause(2000);
     });
 
 });
--- JQueryProject/wdio.conf.js
+++ JQueryProject/wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './tests/login.spec.js'
+        './tests/intro.spec.js'
     ],
     // Patterns to exclude.
     exclude: [
