3d20875ca30362209e4f1aadc2530eaecbc38b17
Martin <marb61a@gmail.com>
2020-12-22 11:46:47 +0000

    Updated smoke spec test

--- JQueryProject/tests/smoke.spec.js
+++ JQueryProject/tests/smoke.spec.js
@@ -12,14 +12,34 @@ describe('Smoke Test Suite', () => {
 
         browser.maximizeWindow();
 
-        LoginPage.emailField.setValue('1@2.com');
-        LoginPage.passwordField.setValue('password');
-        LoginPage.rememberLoginCheckbox.click();
-        LoginPage.submitButton.click();
+        // Verifying the Login
+        assert.equal(LoginPage.headingText.isDisplayed(), true, "LoginPage.headingText");
+        assert.equal(LoginPage.emailLabel.isDisplayed(), true, "LoginPage.emailLabel");
+        assert.equal(LoginPage.emailField.isDisplayed(), true, "LoginPage.emailField");
+        assert.equal(LoginPage.passwordLabel.isDisplayed(), true, "LoginPage.passwordLabel");
+        assert.equal(LoginPage.passwordField.isDisplayed(), true, "LoginPage.passwordField");
+        assert.equal(LoginPage.rememberLoginCheckbox.isDisplayed(), true, "LoginPage.rememberLoginCheckbox");
+        assert.equal(LoginPage.rememberLoginLabel.isDisplayed(), true, "LoginPage.rememberLoginLabel");
+        assert.equal(LoginPage.submitButton.isDisplayed(), true, "LoginPage.submitButton");
+
+        // Header Verification
+        assert.equal(HeaderPage.logoutLink.isDisplayed(), true, "HeaderPage.logoutLink");
+        assert.equal(HeaderPage.linkLink.isDisplayed(), true, "HeaderPage.linkLink");
+        assert.equal(HeaderPage.heroFactsLink.isDisplayed(), true, "HeaderPage.heroFactsLink");
+        assert.equal(HeaderPage.wolverineOption.isDisplayed(), true, "HeaderPage.wolverineOption");
+        assert.equal(HeaderPage.spiderManOption.isDisplayed(), true, "HeaderPage.spidermanOption");
+        assert.equal(HeaderPage.searchField.isDisplayed(), true, "HeaderPage.searchField");
+        assert.equal(HeaderPage.searchButton.isDisplayed(), true, "HeaderPage.searchButton");
+
+        // Intro Verification
+        assert.equal(IntroPage.titleText.isDisplayed(), true, "IntroPage.titleText");
+        assert.equal(IntroPage.mainImage.isDisplayed(), true, "IntroPage.mainImage");
+
+        // Roster Verification
+        assert.equal(RosterPage.instructionText.isDisplayed(), true, "RosterPage.instructionText");
+        assert.equal(RosterPage.listTitle.isDisplayed(), true, "RosterPage.listTitle");
 
         bowser.pause(2000);
     });
-
-    it('Should test something else');
 
 });
