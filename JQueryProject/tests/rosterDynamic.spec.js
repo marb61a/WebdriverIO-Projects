19edba31b3353168d7cdc08afa1359b7ca9054b7
Martin <marb61a@gmail.com>
2021-01-24 12:03:51 +0000

    Updated rosterDynamic spec file

--- JQueryProject/tests/rosterDynamic.spec.js
+++ JQueryProject/tests/rosterDynamic.spec.js
@@ -4,7 +4,7 @@ const LoginPage = require('../pages/login.page');
 const RosterPage = require('../pages/roster.page');
 
 Describe("Dynamic roster item suite", () => {
-    it("should create multiple items", () => {
+    it("should create single item", () => {
         browser.url('');
 
         browser.maximizeWindow();
@@ -16,7 +16,39 @@ Describe("Dynamic roster item suite", () => {
         RosterPage.addHeroField.setValue("Spongebob");
         RosterPage.submitButton.click();
 
-        assert.equal(RosterPage.rosterItems[6].getText(), "Spongebob", "Hero text does not match");
-        
+        assert.equal(RosterPage.rosterItems[5].getText(), "Spongebob", "Hero text does not match");
+
+    });
+
+    it("should have default list of heroes", () => {
+        let heroes = ["Wolverine", "Iron Man", "Deadpool", "Thor", "Spider-Man"];
+
+        browser.url('');
+
+        browser.maximizeWindow();
+
+        LoginPage.emailField.setValue('1@2.com');
+        LoginPage.passwordField.setValue('password');
+        LoginPage.submitButton.click();
+
+        for(let i = 0; i < heroes.length; i++){
+            assert.equal(RosterPage.rosterItems[i].getText(), heroes[i], "Hero text does not match");
+        }
+    });
+
+    it("should create single item", () => {
+        let heroes = ["Joe", "Tom", "Dave", "Sharon"];
+
+        browser.url('');
+
+        browser.maximizeWindow();
+
+        LoginPage.emailField.setValue('1@2.com');
+        LoginPage.passwordField.setValue('password');
+        LoginPage.submitButton.click();
+
+        for(let i = 0; i < heroes.length; i++){
+            assert.equal(RosterPage.rosterItems[i].getText(), heroes[i], "Hero text does not match");
+        }
     });
 });
